138d9152b8adda3f31752a54119088f0b710c0c6
dotm <desde.el.mar96@gmail.com>
2019-10-26 14:25:08 +0700

    add wifi router address finder

---
+++ src/wifi-router-address-finder/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func main() {
+	pingPossibleWiFiRouterAddresses()
+}
+
+func pingPossibleWiFiRouterAddresses() {
+	var addressList []string
+	readFromFile(getPathToTextList(), func(s string) { addressList = append(addressList, s) })
+	wg := sync.WaitGroup{}
+	for _, address := range addressList {
+		wg.Add(1)
+		go pingAddress(address, &wg)
+	}
+	wg.Wait()
+}
+
+func pingAddress(address string, wg *sync.WaitGroup) {
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	res, err := client.Head("http://" + address)
+	if err != nil {
+		//fmt.Println("failed to ping", address, err)
+		wg.Done()
+		return
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusOK {
+		fmt.Println(address, "responded with", http.StatusOK)
+	}
+	wg.Done()
+}
+
+func readFromFile(path string, cb func(string)) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		cb(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getPathToTextList() (path string) {
+	//default to accounts.json
+	dir, _ := getPathToThisFile()
+	path = dir + "/address-list.txt"
+	return
+}
+
+func getPathToThisFile() (path string, err error) {
+	_, path, _, _ = runtime.Caller(1)
+	path = filepath.Dir(path)
+	return
+}
